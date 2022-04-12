aded16975a475a8f56e3193829a24a4b4c8cb8bc
Mikhail Subach <mikhail_subach@epam.com>
2022-04-12 13:18:14 +0400

    template updated with the button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,19 @@
+<script setup>
+
+import {ref} from 'vue'
+
+let username = localStorage.getItem('user.lichess.username') || "";
+
+function getInsights() {
+  console.log(username);
+}
+
+</script>
+
 <template>
   <h1>Game Insights</h1>
-  <div>Username: <input id="username" type="text"/></div>
+  <div>Lichess.org username: 
+    <input id="username"  type="text" v-model="username"/>
+    <button @click="getInsights">Get Insights</button>
+  </div>
 </template>
