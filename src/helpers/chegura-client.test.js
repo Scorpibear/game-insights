d32bb983d2456e80a9d692b2a35318e467302d26
Mikhail Subach <mikhail_subach@epam.com>
2022-05-05 11:43:37 +0300

    linter configuration added

--- src/helpers/chegura-client.test.js
+++ src/helpers/chegura-client.test.js
@@ -1,11 +1,15 @@
-import { describe, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import { CheguraClient } from "./chegura-client";
 
-describe('CheguraClient', () => {
-  describe('getBaseUrl', () => {
-    test('use protocol, hostname and port', () => {
-      let cheguraClient = new CheguraClient({hostname: 'testhost', protocol: 'https', port: 87654})
-      expect(cheguraClient.getBaseUrl()).toBe('https://testhost:87654/api/');
-    })
-  })
+describe("CheguraClient", () => {
+  describe("getBaseUrl", () => {
+    it("uses protocol, hostname and port", () => {
+      let cheguraClient = new CheguraClient({
+        hostname: "testhost",
+        protocol: "https",
+        port: 87654,
+      });
+      expect(cheguraClient.getBaseUrl()).toBe("https://testhost:87654/api/");
+    });
+  });
 });
