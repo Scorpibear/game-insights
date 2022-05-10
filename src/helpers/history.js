447bd5b97229a49f09b033d7ce18a829a1f9a429
Mikhail Subach <mikhail_subach@epam.com>
2022-05-10 17:22:09 +0300

    key expiration check

--- src/helpers/history.js
+++ src/helpers/history.js
@@ -1,12 +1,22 @@
+const defaultTTL = 1000 * 60 * 60 * 24 * 30;
+
 export default class History {
   constructor(options) {
     this.map = new Map();
     this.options = options || {};
   }
   get(key) {
-    return this.map.get(key);
+    const data = this.map.get(key);
+    return data
+      ? Date.now() < data.expireAt
+        ? data.value
+        : undefined
+      : undefined;
   }
   set(key, value) {
-    return this.map.set(key, value);
+    return this.map.set(key, {
+      value,
+      expireAt: Date.now() + (this.options.ttl || defaultTTL),
+    });
   }
 }
--- src/helpers/history.test.js
+++ src/helpers/history.test.js
@@ -7,4 +7,14 @@ describe("History", () => {
     history.set("keyA", 42);
     expect(history.get("keyA")).toBe(42);
   });
+  it("makes a key value expired if ttl is specified", async () => {
+    let history = new History({ ttl: 1 });
+    history.set("myKey", "value5");
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 2);
+    });
+    expect(history.get("myKey")).toBeUndefined();
+  });
 });
