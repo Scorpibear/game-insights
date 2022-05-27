cca8fa5a68f74da62dc00fec9aa7a7b17d889699
Mikhail Subach <mikhail_subach@epam.com>
2022-05-27 14:05:38 +0300

    lichess.getCloudEval added

--- src/helpers/lichess-client.js
+++ src/helpers/lichess-client.js
@@ -1,7 +1,16 @@
 export class LichessClient {
   async getTheMostPopularByMasters(fen) {
     const url = `https://explorer.lichess.ovh/masters?fen=${fen}`;
-    const response = await fetch(url, {headers: {Accept:'application/json' }});
+    const response = await fetch(url, {
+      headers: { Accept: "application/json" },
+    });
+    return response.json();
+  }
+  async getCloudEval(fen) {
+    const url = `https://lichess.org/api/cloud-eval?fen=${fen}`;
+    const response = await fetch(url, {
+      headers: { Accept: "application/json" },
+    });
     return response.json();
   }
 }
--- src/helpers/lichess-client.test.js
+++ src/helpers/lichess-client.test.js
@@ -1,13 +1,28 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { LichessClient } from "./lichess-client";
 
-describe('lichess client', () => {
+describe("lichess client", () => {
   const lichessClient = new LichessClient();
-  describe('getTheMostPopularByMasters', () => {
-    test('fetch endpoint: https://explorer.lichess.ovh/masters', () => {
-      vi.spyOn(global, 'fetch').mockImplementation(() => ({json: () => {}}));
-      lichessClient.getTheMostPopularByMasters('TESTFEN');
-      expect(fetch).toHaveBeenCalledWith('https://explorer.lichess.ovh/masters?fen=TESTFEN', {headers: {Accept:'application/json' }});
-    })
-  })
-})
+  describe("getTheMostPopularByMasters", () => {
+    it("fetches endpoint: https://explorer.lichess.ovh/masters", () => {
+      vi.spyOn(global, "fetch").mockImplementation(() => ({ json: () => {} }));
+      lichessClient.getTheMostPopularByMasters("TESTFEN");
+      expect(fetch).toHaveBeenCalledWith(
+        "https://explorer.lichess.ovh/masters?fen=TESTFEN",
+        { headers: { Accept: "application/json" } }
+      );
+    });
+  });
+  describe("getCloudEval", () => {
+    it("fetches endpoint https://lichess.org/api/cloud-eval", async () => {
+      vi.spyOn(global, "fetch").mockResolvedValue({
+        json: () => Promise.resolve({}),
+      });
+      await lichessClient.getCloudEval("TESTFEN");
+      expect(fetch).toHaveBeenCalledWith(
+        "https://lichess.org/api/cloud-eval?fen=TESTFEN",
+        { headers: { Accept: "application/json" } }
+      );
+    });
+  });
+});
