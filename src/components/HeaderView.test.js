c6a39dc1758ddcd848f87207f320f9399fc196ca
Mikhail Subach <mikhail_subach@epam.com>
2022-07-25 12:54:24 +0300

    minor refactoring of HeaderView

--- src/components/HeaderView.test.js
+++ src/components/HeaderView.test.js
@@ -1,13 +1,18 @@
 import { render } from "@testing-library/vue";
-import { it } from "vitest";
+import { describe, it } from "vitest";
 
 import HeaderView from "./HeaderView.vue";
 
-it("requests lichess username", () => {
-  const { getByText } = render(HeaderView, {
-    props: {},
-  });
+describe("HeaderView", () => {
+  it("requests lichess username", () => {
+    const { getByText } = render(HeaderView, {
+      props: {},
+    });
 
-  // assert output
-  getByText("Game insights for:");
+    // assert output
+    getByText("Game insights for:");
+  });
+  describe("get button", () => {
+    it("request the latest games from backend", () => {});
+  });
 });
