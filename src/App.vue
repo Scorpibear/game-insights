f94b9388165f4045ae681e69e5f75442c531ff68
Mikhail Subach <mikhail_subach@epam.com>
2022-08-19 14:03:16 +0300

    4 popular games for both sides

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,14 @@ import BoardView from "./components/BoardView.vue";
 const state = reactive({
   username: "",
   games: [
-    { pgn: "1. e4 c6 2. Nf3 d5 3. Nc3 ?-?" },
+    { pgn: "1. e4 c5 2. Nf3 d6 3. Nc3 Nf6 4. d4 cxd4 5. Nxd4 a6 6. ?-?" },
+    {
+      pgn: "1. e4 c6 2. d4 d5 3. Nc3 dxe4 4. Nxe4 Bf5 5. Ng3 Bg6 6. h4 h6 7.Nf3 Nd7 8. h5 Bh7 9. Bd3 Bxd3 10. Qxd3 ?-?",
+    },
+    {
+      pgn: "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Be7 6. ?-?",
+    },
     { pgn: "1. d4 Nf6 2. c4 e6 3. Nf3 d5 4. Nc3 ?-?" },
-    { pgn: "1. Nf3 d5 2. g3 Nf6 3. Bg2 ?-?" },
   ],
 });
 </script>
