41fea1d4459c221e4bf49044f5942a3c562215bc
Mikhail Subach <mikhail_subach@epam.com>
2023-04-04 20:39:36 +0400

    version is displayed (issue #58)

---
+++ src/components/FooterView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { version } from "../../package";
+import { ref } from "vue";
+
+const appVersion = ref(version);
+</script>
+<template>
+  <footer>
+    <a href="https://github.com/Scorpibear/game-insights"
+      >Game Insights v{{ appVersion }}</a
+    >
+  </footer>
+</template>
+<style>
+footer {
+  font-size: xx-small;
+}
+</style>
