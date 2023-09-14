05ce4c067b9c6ac21a48be03e9f6a8fe5041b00c
Mikhail Subach <mikhail_subach@epam.com>
2023-09-14 14:39:00 +0300

    fix handling disconnect issues while get popular

--- src/clients/lichess-client.test.js
+++ src/clients/lichess-client.test.js
@@ -7,26 +7,36 @@ describe("lichess client", () => {
   const lichessClient = new LichessClient();
   describe("getTheMostPopularByMasters", () => {
     it("fetches endpoint: https://explorer.lichess.ovh/masters", () => {
-      spyOn(global, "fetch").mockImplementation(() => ({ json: () => {} }));
+      spyOn(global, "fetch").mockResolvedValue({ json: () => {} });
       lichessClient.getTheMostPopularByMasters("TESTFEN");
       expect(fetch).toHaveBeenCalledWith(
         "https://explorer.lichess.ovh/masters?fen=TESTFEN",
         { headers: { Accept: "application/json" } }
       );
     });
+    it("handles disconnects", async () => {
+      spyOn(global, "fetch").mockRejectedValue("ERR_INTERNET_DISCONNECTED");
+      const output = await lichessClient.getTheMostPopularByMasters("TESTFEN");
+      expect(output).toBeNull();
+    })
     afterEach(() => {
       vi.restoreAllMocks();
     });
   });
   describe("getTheMostPopularOnline", () => {
     it("fetches endpoint: https://explorer.lichess.ovh/lichess", () => {
-      spyOn(global, "fetch").mockImplementation(() => ({ json: () => {} }));
+      spyOn(global, "fetch").mockResolvedValue({ json: () => {} });
       lichessClient.getTheMostPopularOnline("TESTFEN");
       expect(fetch).toHaveBeenCalledWith(
         "https://explorer.lichess.ovh/lichess?variant=standard&speeds=blitz,rapid,classical,correspondence&ratings=2000,2200,2500&fen=TESTFEN",
         { headers: { Accept: "application/json" } }
       );
     });
+    it("handles disconnects", async () => {
+      spyOn(global, "fetch").mockRejectedValue("ERR_INTERNET_DISCONNECTED");
+      const output = await lichessClient.getTheMostPopularOnline("TESTFEN");
+      expect(output).toBeNull();
+    })
     afterEach(() => {
       vi.restoreAllMocks();
     });
